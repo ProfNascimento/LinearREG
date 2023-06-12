f0dd8ae059666f15e5a5e16d119e9e5fc08fb298
Diego Nascimento <91238394+ProfNascimento@users.noreply.github.com>
2023-06-12 11:20:29 -0400

    Add files via upload

---
+++ Data_HSS.R
@@ -0,0 +1,49 @@
+#----------------------------------------------------------------------------------#
+## https://www.kaggle.com/datasets/harrimansaragih/dummy-advertising-and-sales-data
+# Data of TV, Influencer, Radio, and Social Media Ads budget to predict Sales
+#----------------------------------------------------------------------------------#
+
+## DATA IMPORT
+HSS <- read.csv("https://raw.githubusercontent.com/ProfNascimento/LinearREG/main/Data_HSS.csv")
+str(HSS)
+
+summary(HSS)
+unique(HSS$Influencer)
+
+## VISUALIZATION 
+require(psych)
+pairs.panels(
+  HSS[,c(1:3,5)],
+  method = "pearson", # correlation method
+  hist.col = "#00AFBB",
+  density = TRUE,  # show density plots
+  ellipses = TRUE) # show correlation ellipses
+
+## VISUALIZATION NAs
+Amelia::missmap(HSS)
+dev.off()
+
+## CORRELATION AMONG FEATURES
+cor(na.omit(HSS[,c(1:3,5)]))
+
+## MODELO CON INTERCEPTO
+fit=lm(Sales ~ ., data=HSS)
+summary(fit)
+
+## DUMMY DATASET
+DB=fastDummies::dummy_cols(HSS,select_columns = "Influencer",remove_selected_columns = TRUE)
+head(DB)
+
+## MODELO SIN INTERCEPTO
+fit2=lm(Sales ~ 0+., data=DB)
+summary(fit2)
+
+## RESIDUO DE LOS MODELOS
+hist(residuals(fit),xlab="Modelo 1")
+
+hist(residuals(fit2),xlab="Modelo 2")
+car::qqPlot(residuals(fit2))
+
+#------------------------------------------------#
+## ANOTHER WAY TO GARANTEE GENERALIZATION (HOLD-OUT)
+## [80% TRAIN - 20% TEST]
