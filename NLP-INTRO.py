9c8648b1001d0c10a1f27a6fc5d2ed1dc0cbb00c
Diego Nascimento <91238394+ProfNascimento@users.noreply.github.com>
2023-06-19 00:23:16 -0400

    Add files via upload

---
+++ NLP-INTRO.py
@@ -0,0 +1,193 @@
+"""
+@author: Prof. Diego Nascimento
+Dep. Matematica - UDA
+"""
+# Import Libraries
+import pandas as pd
+from tqdm import tqdm
+tqdm.pandas()
+pd.options.display.max_colwidth = 150
+import time
+
+import re
+from multiprocessing import Pool
+
+from keras.preprocessing.text import text_to_word_sequence
+
+import nltk
+from nltk import pos_tag
+from nltk.corpus import stopwords
+from nltk.corpus import wordnet
+from nltk.stem import WordNetLemmatizer
+nltk.download('omw-1.4')
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+import warnings
+warnings.filterwarnings("ignore")
+
+list_colors = ["#f26a37", "#DFB15B", "#706695"]
+
+
+## IMPORT DATASET
+women_clothes_reviews = pd.read_csv("https://raw.githubusercontent.com/ProfNascimento/MP/main/Womens%20Clothing%20E-Commerce%20Reviews.csv")
+
+women_clothes_reviews.info()
+
+
+## Text Preprocessing
+def remove_sub_text(pattern, text):
+    pattern = re.compile(pattern) 
+    clean_text = re.sub(pattern, ' ', text)
+    return clean_text
+    
+def pos_tagging(token):
+    pos_dict = {'J':wordnet.ADJ, 'V':wordnet.VERB, 'N':wordnet.NOUN, 'R':wordnet.ADV}
+    tags = pos_tag(token)
+    newlist = []
+    for word, tag in tags:
+        newlist.append(tuple([word, pos_dict.get(tag[0])]))
+    return newlist
+
+def lemmatize(pos_data):
+    wordnet_lemmatizer = WordNetLemmatizer()
+    lemma_rew = " "
+    for word, pos in pos_data:
+        if not pos:
+            lemma = word
+            lemma_rew = lemma_rew + " " + lemma
+        else:
+            lemma = wordnet_lemmatizer.lemmatize(word, pos=pos)
+            lemma_rew = lemma_rew + " " + lemma
+    return lemma_rew
+
+
+women_clothes_reviews.shape
+
+# Remove NaN Text
+women_clothes_reviews = women_clothes_reviews.dropna(subset=["Review Text"])
+
+# Remove URL
+url_pattern = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+women_clothes_reviews["Clean Text"] = women_clothes_reviews["Review Text"].progress_apply(lambda x: re.sub(url_pattern, ' ', x))
+print("Remove URL: Okay")
+
+# Remove HTML
+pattern = '<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});'
+women_clothes_reviews["Clean Text"] = women_clothes_reviews["Clean Text"].progress_apply(lambda x: remove_sub_text(pattern, x))
+print("Remove HTML: Okay")
+
+# Remove Number
+women_clothes_reviews["Clean Text"] = women_clothes_reviews["Clean Text"].progress_apply(lambda x: ''.join(s for s in x if not s.isdigit()))
+print("Remove Number: Okay")
+
+# Remove Extra Spaces
+women_clothes_reviews["Clean Text"] = women_clothes_reviews["Clean Text"].progress_apply(lambda x: ' '.join(x.split()))
+print("Remove Extra Spaces: Okay")
+
+# Lower Casing
+women_clothes_reviews["Clean Text"] = women_clothes_reviews["Clean Text"].progress_apply(lambda x: x.lower())
+print("Lower Casing: Okay")
+
+# Tokenization
+women_clothes_reviews["Token Text"] = women_clothes_reviews["Clean Text"].progress_apply(lambda x: text_to_word_sequence(x))
+print("Tokenization: Okay")
+
+# Remove Stopwords
+stop = set(stopwords.words('english'))
+women_clothes_reviews["Token Text"] = women_clothes_reviews["Token Text"].progress_apply(lambda x: [word for word in x if word not in stop])
+print("Remove Stopwords: Okay")
+
+# POS Tagging
+start_time = time.time()
+pos_tagging_result = []
+with Pool() as pool:
+    result = pool.map_async(pos_tagging, women_clothes_reviews['Token Text'])
+    for result in result.get():
+        pos_tagging_result.append(result)
+    pool.close()
+    pool.join()
+women_clothes_reviews["POS Tagged"] = pos_tagging_result
+print("--- %s seconds ---" % (time.time() - start_time))
+print("POS Tagging: Okay")
+
+# Lemmatization
+women_clothes_reviews['Final Text'] = women_clothes_reviews['POS Tagged'].progress_apply(lemmatize)
+print("Lemmatization")
+
+# Remove Extra Spaces Again
+women_clothes_reviews["Final Text"] = women_clothes_reviews["Final Text"].progress_apply(lambda x: ' '.join(x.split()))
+print("Remove Extra Spaces Again: Okay")
+
+# Remove NaN Final Text Again
+print("Before Remove NaN Final Text:", women_clothes_reviews.shape)
+women_clothes_reviews = women_clothes_reviews.dropna(subset=["Final Text"])
+print(" After Remove NaN Final Text:", women_clothes_reviews.shape)
+print("Remove NaN Final Text: Okay")
+
+# Remove Final Text with Only 1 Word
+women_clothes_reviews["Word Count"] = women_clothes_reviews["Final Text"].progress_apply(lambda x: len(x.split()))
+print("Before Remove Only 1 Word:", women_clothes_reviews.shape)
+women_clothes_reviews = women_clothes_reviews[women_clothes_reviews["Word Count"]>1]
+print(" After Remove Only 1 Word:", women_clothes_reviews.shape)
+print("Remove Final Text with Only 1 Word: Okay")
+
+## FIRST LOOK
+women_clothes_reviews.head()
+
+# women_clothes_reviews.to_csv("clean_women_clothes_reviews.csv", index=False)
+
+women_clothes_reviews.set_option('max_colwidth', 500)
+# PRINT 3 SAMPLES
+women_clothes_reviews[["Title","Final Text", "Rating",'Recommended IND']].sample(3)
+
+## VISUALIZATION - WORDCLOUD
+import matplotlib as mpl
+from wordcloud import WordCloud, STOPWORDS
+stopwords = list(STOPWORDS)
+fieldnames = list(women_clothes_reviews['Class Name'].unique())
+
+def cloud(text, title, stopwords=stopwords, size=size):
+    # Setting figure parameters
+    mpl.rcParams['figure.figsize']=(10.0,10.0)
+    mpl.rcParams['font.size']=12
+    mpl.rcParams['savefig.dpi']=100
+    mpl.rcParams['figure.subplot.bottom']=.1 
+    
+    wordcloud = WordCloud(width=1600, height=800,
+                          background_color='black',
+                          stopwords=stopwords,
+                         ).generate(str(text))
+    
+    fig = plt.figure(figsize=size, dpi=80, 
+                     facecolor='k',edgecolor='k')
+    plt.imshow(wordcloud,interpolation='bilinear')
+    plt.axis('off')
+    plt.title(title, fontsize=50,color='y')
+    plt.tight_layout(pad=0)
+    plt.show()
+
+cloud(text= women_clothes_reviews.Title.astype(str).values,
+      title="titles",
+      stopwords= stopwords,
+      size = (7,4))
+
+# VISUALIZATION MOST FREQ. WORD PER REVIEW CLASS (WORDCLOUD)
+a1 = women_clothes_reviews[women_clothes_reviews['Rating']>=3]
+cloud(text = a1['Final Text'].astype(str).values,
+      title ='',
+      stopwords = stopwords,
+      size = (7,4))
+
+stopwords2 = list(STOPWORDS) + ["dress", "petite","made","will"]
+cloud(text = a1['Final Text'].astype(str).values,
+      title ='',
+      stopwords = stopwords2,
+      size = (7,4))
+
+a2 = women_clothes_reviews[women_clothes_reviews['Rating']<3]
+cloud(text = a2['Final Text'].astype(str).values,
+      title ='Negative Review Text',
+      stopwords = stopwords,
+      size = (7,4))
---
+++ NLP-LOGIST_REG.py
@@ -0,0 +1,69 @@
+## LOGISTIC REGRESSION
+## REVIEW CLASSIFICATION BASED ON WORDS
+
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+ 
+vectorizer = CountVectorizer()
+
+## Fit the bag-of-words model
+bag = vectorizer.fit_transform(women_clothes_reviews['Final Text'])
+
+# Creating training data set from bag-of-words and dummy label
+X = bag.toarray()
+y = np.array(women_clothes_reviews['Recommended IND'])
+ 
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
+
+# Create training and test split
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+# Create an instance of LogisticRegression classifier
+lr = LogisticRegression(C=100.0, random_state=1, solver='lbfgs', multi_class='ovr')
+
+# Fit the model
+model=lr.fit(X_train, y_train)
+
+# Create the predictions
+y_predict = model.predict(X_test)
+y_predict_prob = model.predict_proba(X_test)
+  
+# Use metrics.accuracy_score to measure the score
+print("LogisticRegression Accuracy %.3f" %metrics.accuracy_score(y_test, y_predict))
+
+#################################
+from sklearn.metrics import confusion_matrix
+confusion_matrix(y_test,y_predict)
+
+## Self-defined threshold
+y_predict_self = [1 if prob > 0.041926 else 0 for prob in y_predict_prob[:,1]]
+# Model evaluation on accuracy
+confusion_matrix(y_test,y_predict_self)
+
+
+from sklearn.metrics import roc_curve
+from matplotlib import pyplot
+
+fpr, tpr, thresholds = roc_curve(y_test, y_predict_prob[:,1])
+# plot the roc curve for the model
+pyplot.plot([0,1], [0,1], linestyle='--', label='No Skill')
+pyplot.plot(fpr, tpr, marker='.', label='Logistic')
+# axis labels
+pyplot.xlabel('False Positive Rate')
+pyplot.ylabel('True Positive Rate')
+pyplot.legend()
+# show the plot
+pyplot.show()
+
+## Optimal THRESHOLD BASED ON F1-SCORE 
+from numpy import argmax
+from sklearn.metrics import precision_recall_curve
+# calculate roc curves
+precision, recall, thresholds = precision_recall_curve(y_test, y_predict_prob[:,1])
+# convert to f score
+fscore = (2 * precision * recall) / (precision + recall)
+# locate the index of the largest f score
+ix = argmax(fscore)
+print('Best Threshold=%f, F-Score=%.3f' % (thresholds[ix], fscore[ix]))
